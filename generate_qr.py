3841ee186486150a8e14167a7b3053560eb45870
Kevin <kmanc@comcast.net>
2018-05-25 17:07:22 -0700

    generate a qr code for the wifi password string

---
+++ generate_qr.py
@@ -0,0 +1,31 @@
+import configparser
+import ssl
+import urllib.parse
+import urllib.request
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+wifi_password = config['WIFI']['password']
+
+
+def make_qr_code(text):
+    """Create a computer readable QR code in a PNG format using Google Chart REST API"""
+    # https://developers.google.com/chart/infographics/docs/qr_codes
+    root_url = 'https://chart.googleapis.com/chart?'
+    query = dict(cht='qr', chs='300x300', chl=text)
+    url = root_url + urllib.parse.urlencode(query)
+    with urllib.request.urlopen(url, context=ssl.SSLContext()) as u:
+        image = u.read()
+    # According to www.w3.org/TR/PNG/#5PNG-file-signature, first 8 bytes should match
+    # 137 80 78 71 13 10 26 10
+    try:
+        assert image[:8] == bytes([137, 80, 78, 71, 13, 10, 26, 10])
+        return image
+    except AssertionError:
+        print('The QR code failed to generate correctly')
+        exit(0)
+
+
+qr = make_qr_code(wifi_password)
+with open('wifi_qr.png', 'wb') as image:
+    image.write(qr)
