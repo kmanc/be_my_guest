8860c7d2d358a994f560c0a96dc74aa55ba01769
Kevin <kmancxc@gmail.com>
2021-02-17 20:52:34 -0800

    accomodate new networking changes to unifi

--- network_update.py
+++ network_update.py
@@ -9,15 +9,15 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 config.read(f'{dir_path}/config.ini')
 
 administration_host = config["NETWORK_ADMINISTRATION"]["host"]
-login_username = config["NETWORK_ADMINISTRATION"]["user"]
+login_username = config["NETWORK_ADMINISTRATION"]["username"]
 login_password = config["NETWORK_ADMINISTRATION"]["password"]
 wifi_id = config["WIFI"]["id"]
 wifi_new_password = config["PASSWORD"]["value"]
 
-url = f"https://{administration_host}:8443/api/login"
+url = f"https://{administration_host}:443/api/auth/login"
 payload = {"username": login_username, "password": login_password}
 
-login = requests.post(url, json=payload)
+login = requests.post(url, json=payload, verify=False)
 
 try:
     assert login.status_code == 200
@@ -25,26 +25,20 @@ except AssertionError:
     print("Problem logging in")
     sys.exit()
 
-
-cookie_parsing = login.headers["Set-Cookie"].split(" ")
-for entry in cookie_parsing:
-    if "unifises" in entry:
-        unifises = entry.split("=")[1]
-    if "csrf_token" in entry:
-        csrf_token = entry.split("=")[1][:-1]
+token = login.headers.get("Set-Cookie").split(";")[0]
+csrf_token = login.headers.get("x-csrf-token")
 
 try:
-    assert "unifises" in dict(globals())
-    assert "csrf_token" in dict(globals())
+    assert token is not None
+    assert csrf_token is not None
 except AssertionError:
     print("Problem parsing login response for tokens")
     sys.exit()
 
-url = f"https://{administration_host}:8443/api/s/default/rest/wlanconf/{wifi_id}"
+url = f"https://{administration_host}:443/proxy/network/api/s/default/rest/wlanconf/{wifi_id}"
 payload = {"x_passphrase": wifi_new_password}
-headers = {"Cookie": f"unifises={unifises} csrf_token={csrf_token}"}
-
-change = requests.put(url, json=payload, headers=headers)
+headers = {"Cookie": token, "x-csrf-token": csrf_token}
+change = requests.put(url, json=payload, headers=headers, verify=False)
 
 try:
     assert change.status_code == 200
