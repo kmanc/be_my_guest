d90635c5fc12208b40913f93e8c9291a214caf6e
Kevin <kmancxc@gmail.com>
2021-10-24 21:25:08 -0700

    is this better? its the same thing written differently

--- network_update.py
+++ network_update.py
@@ -14,45 +14,47 @@ login_password = config["NETWORK_ADMINISTRATION"]["password"]
 wifi_id = config["WIFI"]["id"]
 wifi_new_password = config["PASSWORD"]["value"]
 
-url = f"https://{administration_host}:443/api/auth/login"
-payload = {"username": login_username, "password": login_password}
 
-login = requests.post(url, json=payload, verify=False)
+def create_client(login_url, login_payload):
+    session = requests.Session()
+    session.verify = False
+    login = session.post(login_url, json=login_payload, verify=False)
+    if login.status_code != 200:
+        print("Problem logging in")
+        sys.exit()
 
-try:
-    assert login.status_code == 200
-except AssertionError:
-    print("Problem logging in")
-    sys.exit()
+    token_needs_parsing = login.headers.get("Set-Cookie")
 
-token_needs_parsing = login.headers.get("Set-Cookie")
+    try:
+        token = token_needs_parsing.split(";")[0]
+    except AttributeError:
+        print("Set-Cookie token not found in login response")
+        sys.exit()
+    except SyntaxError:
+        print("Token value found is not a string")
+        sys.exit()
 
-try:
-    assert token_needs_parsing is not None
-    token = token_needs_parsing.split(";")[0]
-except AssertionError:
-    print("Set-Cookie token not found in login response")
-    sys.exit()
-except SyntaxError:
-    print("Token value found is not a string")
-    sys.exit()
+    csrf_token = login.headers.get("x-csrf-token")
 
-csrf_token = login.headers.get("x-csrf-token")
+    if csrf_token is None:
+        print("CSRF token not found in login response")
+        sys.exit()
 
-try:
-    assert csrf_token is not None
-except AssertionError:
-    print("CSRF token not found in login response")
-    sys.exit()
+    session.headers.update({"Cookie": token, "x-csrf-token": csrf_token})
 
-url = f"https://{administration_host}:443/proxy/network/api/s/default/rest/wlanconf/{wifi_id}"
-payload = {"x_passphrase": wifi_new_password}
-headers = {"Cookie": token, "x-csrf-token": csrf_token}
-change = requests.put(url, json=payload, headers=headers, verify=False)
+    return session
 
-try:
-    assert change.status_code == 200
+
+auth_endpoint = f"https://{administration_host}:443/api/auth/login"
+auth_params = {"username": login_username, "password": login_password}
+unifi_client = create_client(auth_endpoint, auth_params)
+network_url = f"https://{administration_host}:443/proxy/network/api/s/default/rest/wlanconf/{wifi_id}"
+network_payload = {"x_passphrase": wifi_new_password}
+
+change = unifi_client.put(network_url, json=network_payload)
+
+if change.status_code == 200:
     print("Network password successfully updated")
-except AssertionError:
+else:
     print("Network password update failed")
 
