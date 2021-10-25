a25aff90a3c8a6d48b25420495415811ecd4bc9d
Kevin <kmancxc@gmail.com>
2021-10-25 11:18:03 -0700

    this gets closer to being able to support more network setups

--- network_update.py
+++ network_update.py
@@ -22,39 +22,44 @@ class UnifiInstantiationError(Exception):
     pass
 
 
-def create_unifi_client(login_url, login_payload, verify_requests=False):
-    session = requests.Session()
-    session.verify = verify_requests
-    login = session.post(login_url, json=login_payload)
-    if login.status_code != 200:
-        raise UnifiAuthorizationError("Problem logging in to the cloud key")
-
-    try:
-        cookie = login.headers.get("Set-Cookie").split(";")[0]
-    except AttributeError:
-        raise UnifiInstantiationError("Set-Cookie token not found in login response")
-    except SyntaxError:
-        raise UnifiInstantiationError("Token value found is not a string")
-
-    csrf_token = login.headers.get("x-csrf-token")
-    if csrf_token is None:
-        raise UnifiInstantiationError("CSRF token not found in login response")
-
-    session.headers.update({"Cookie": cookie, "x-csrf-token": csrf_token})
-
-    return session
+class UnifiActionError(Exception):
+    pass
 
 
-auth_endpoint = f"https://{administration_host}:443/api/auth/login"
-auth_params = {"username": login_username, "password": login_password}
-unifi_client = create_unifi_client(auth_endpoint, auth_params)
-network_url = f"https://{administration_host}:443/proxy/network/api/s/default/rest/wlanconf/{wifi_id}"
-network_payload = {"x_passphrase": wifi_new_password}
+class UnifiClient:
+    def __init__(self, network_controller, username, password, verify_requests=False):
+        self.controller = network_controller
+        auth_endpoint = f"https://{self.controller}:443/api/auth/login"
+        self.session = requests.Session()
+        self.session.verify = verify_requests
+        login_payload = {"username": username, "password": password}
+        login = self.session.post(auth_endpoint, json=login_payload)
+        if login.status_code != 200:
+            raise UnifiAuthorizationError("Problem logging in to the network controller")
 
-change = unifi_client.put(network_url, json=network_payload)
+        try:
+            cookie = login.headers.get("Set-Cookie").split(";")[0]
+        except AttributeError:
+            raise UnifiInstantiationError("Set-Cookie token not found in login response")
+        except SyntaxError:
+            raise UnifiInstantiationError("Set-Cookie token value found is not a string")
 
-if change.status_code == 200:
-    print("Network password successfully updated")
-else:
-    print("Network password update failed")
+        csrf_token = login.headers.get("x-csrf-token")
+        if csrf_token is None:
+            raise UnifiInstantiationError("CSRF token not found in login response")
 
+        self.session.headers.update({"Cookie": cookie, "x-csrf-token": csrf_token})
+
+    def change_wifi_password(self, network_id, new_password):
+        network_url = f"https://{self.controller}:443/proxy/network/api/s/default/rest/wlanconf/{network_id}"
+        network_payload = {"x_passphrase": new_password}
+
+        response = self.session.put(network_url, json=network_payload)
+
+        if response.status_code != 200:
+            raise UnifiActionError(f"Network '{network_id}' password update failed")
+
+
+unifi_client = UnifiClient(administration_host, login_username, login_password)
+unifi_client.change_wifi_password(wifi_id, wifi_new_password)
+print(f"Network '{wifi_id}' password updated")
