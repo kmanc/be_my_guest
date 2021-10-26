6cf065c034540c265f249f6e02aab0affa5eaafb
Kevin <kmancxc@gmail.com>
2021-10-25 22:28:13 -0700

    change import to be in line with my screen size

--- display_update.py
+++ display_update.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import epd1in54_V2
+import epd4in2
 from PIL import Image, ImageDraw, ImageFont
 
-epd = epd1in54_V2.EPD()
+epd = epd4in2.EPD()
 epd.init()
 epd.Clear(0xFF)
 image = Image.open('wifi_qr.png')
