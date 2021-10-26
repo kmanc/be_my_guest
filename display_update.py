628e58992a3eaa3426ca2be5897f691d553d55c5
kmanc <kmancxc@gmail.com>
2021-10-25 22:32:00 -0700

    Missed a few small things (#22)
    * change import to be in line with my screen size
    
    * repo name was wrong lol
    
    * 4.2 inch screen doesnt take an arg to edp.Clear

--- display_update.py
+++ display_update.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import epd1in54_V2
+import epd4in2
 from PIL import Image, ImageDraw, ImageFont
 
-epd = epd1in54_V2.EPD()
+epd = epd4in2.EPD()
 epd.init()
-epd.Clear(0xFF)
+epd.Clear()
 image = Image.open('wifi_qr.png')
 epd.display(epd.getbuffer(image))
 print("Screen display updated")
