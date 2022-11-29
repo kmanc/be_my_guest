5a1d58eb49e1e7386a74e379fa640aa3affc8812
kmanc <koins@duck.com>
2022-11-28 22:26:47 -0800

    Update arduino_string.py

--- arduino_string.py
+++ arduino_string.py
@@ -4,34 +4,19 @@ sketch_template = Template("""
 #include <avr/pgmspace.h>
 #include "DigiKeyboard.h"
 
-// Check this for preserving RAM http://digistump.com/board/index.php/topic,2554.msg12242.html#msg12242
+// Check this for preserving RAM https://web.archive.org/web/20210814101002/http://digistump.com/board/index.php/topic,2554.msg12242.html
 #define gp( x ) (strcpy_P(buffer, (char*)x))
 char buffer[66];
-
 const char c0[] PROGMEM = "$password";
 
-// Enter
-void en() {
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-}
-
-// Print
-void pr(String text) {
-    DigiKeyboard.print(text);   
-}
-
-// Sleep
-void sl(int time) {
-    DigiKeyboard.delay(time);
-}
-
 void setup() {
-    DigiKeyboard.sendKeyStroke(0);
-    sl(300);
-    pr( gp(c0) );
-    sl(100);
-    en();
-    en();
+    DigiKeyboard.sendKeyPress(0);
+    DigiKeyboard.delay(300);
+    DigiKeyboard.print( gp(c0) );
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    // I don't know why this has to be repeated but something isn't working right
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
   
 void loop() {
